dc9de7c6180e2f3e4713b02551449fa22c478d45
Gagan Arora <arorag2577@gmail.com>
2024-04-30 23:30:24 +0530

    basic login

--- back-end/db/dbConnection.js
+++ back-end/db/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/mydatabase");
+    await mongoose.connect("mongodb://localhost:27017/authDatabase");
     console.log("Connected to MongoDB");
   } catch (err) {
     console.log(err);
--- back-end/server.js
+++ back-end/server.js
@@ -9,6 +9,38 @@ const User = require("./db/user");
 // Middleware for parsing JSON data
 app.use(express.json());
 
+// Register a new user
+app.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    console.log(req.body);
+    const user = new User({ username, password });
+    await user.save();
+    res.status(201).send("User registered successfully");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error registering user");
+  }
+});
+
+// Login
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(400).send("Invalid credentials");
+    }
+    if (user.password !== password) {
+      return res.status(400).send("Invalid credentials");
+    }
+    res.status(200).send("Logged in successfully");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error logging in");
+  }
+});
 connectDB();
 
 app.listen(port, () => {
